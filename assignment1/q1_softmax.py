aed126590c18c98b224a8c2f3b1e75237d207fb9
LmYjQ <liu1quan@qq.com>
2018-10-28 08:46:12 +0800

    q1

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -28,16 +28,24 @@ def softmax(x):
     """
     orig_shape = x.shape
 
-    if len(x.shape) > 1:
+    if len(x.shape) > 1 and x.shape[0]>1:
         # Matrix
         ### YOUR CODE HERE
-        _m = a.max(axis=1)
-        b = a - _m.reshape((3,1))
+        n_row = len(x.shape)
+        _max = x.max(axis=1)
+        _zero = np.exp(x - _max.reshape((n_row,1)))
+        _sum = np.sum(_zero, axis=1).reshape(n_row,1)
+        x = _zero/_sum
         ### END YOUR CODE
     else:
         # Vector
         ### YOUR CODE HERE
-        raise NotImplementedError
+        if x.shape[0] ==1:
+            x.reshape((x.shape[1],))
+        _max = x.max()
+        _zero = np.exp(x - _max)
+        _sum = np.sum(_zero)
+        x = _zero/_sum
         ### END YOUR CODE
 
     assert x.shape == orig_shape
@@ -49,25 +57,25 @@ def test_softmax_basic():
     Some simple tests to get you started.
     Warning: these are not exhaustive.
     """
-    print "Running basic tests..."
+    print("Running basic tests...")
     test1 = softmax(np.array([1,2]))
-    print test1
+    print (test1)
     ans1 = np.array([0.26894142,  0.73105858])
     assert np.allclose(test1, ans1, rtol=1e-05, atol=1e-06)
 
     test2 = softmax(np.array([[1001,1002],[3,4]]))
-    print test2
+    print (test2)
     ans2 = np.array([
         [0.26894142, 0.73105858],
         [0.26894142, 0.73105858]])
     assert np.allclose(test2, ans2, rtol=1e-05, atol=1e-06)
 
     test3 = softmax(np.array([[-1001,-1002]]))
-    print test3
+    print (test3)
     ans3 = np.array([0.73105858, 0.26894142])
     assert np.allclose(test3, ans3, rtol=1e-05, atol=1e-06)
 
-    print "You should be able to verify these results by hand!\n"
+    print("You should be able to verify these results by hand!\n")
 
 
 def test_softmax():
@@ -77,7 +85,7 @@ def test_softmax():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print "Running your tests..."
+    print ("Running your tests...")
     ### YOUR CODE HERE
     raise NotImplementedError
     ### END YOUR CODE
@@ -85,4 +93,4 @@ def test_softmax():
 
 if __name__ == "__main__":
     test_softmax_basic()
-    test_softmax()
+    #test_softmax()
